b0ec7a09910269ecc32ca394dc0b9e51d5f45308
Katerine Stoyanova <katestoyanova9@gmail.com>
2015-08-29 15:12:22 +0300

    I am Kate

---
+++ IntroductionToProgramming02/HelloCSharp/HelloCSharp.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace HelloCSharp
+{
+    using System;
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello,I am Kate");
+            Console.WriteLine("I try to code");
+            Console.WriteLine("yes");
+        }
+    }
+}
